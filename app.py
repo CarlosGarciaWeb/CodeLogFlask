653f2847155917d0023626b3a81e257ab293fe95
Carlos García <carlosgarcia@Carloss-MacBook-Air.local>
2022-11-16 12:43:49 -0500

    incorporate mongodb to flask app with pymongo. find all items in the db created for the log and made an add log option through a post form.

--- app.py
+++ app.py
@@ -1,28 +1,39 @@
-from flask import Flask, render_template, request
 from datetime import datetime
-
-app = Flask(__name__)
-
-entries_list = []
-
-@app.route("/", methods=["GET", "POST"])
-def home_page():
-    if request.method == "POST":
-        log_content = request.form.get("log")
-        log_information = (log_content, datetime.now().strftime("%m/%d/%y"), datetime.now().strptime(datetime.now().strftime("%m/%d/%y"), "%m/%d/%y"))
-        entries_list.append(log_information)
-    kwargs = {
-        "log_info": entries_list,
-    }
-    return render_template("index.html", **kwargs)
-
-
-# Here it will have a dynamic url passing the slug of the thread title
-@app.route("/thread")
-def thread():
-    return render_template("thread.html")
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
+import os
 
 
 
-if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=4000)
+
+
+def create_app():
+    app = Flask(__name__)
+
+    mongo_pass_env = os.environ.get('MONGODB_PASS')
+
+    client = MongoClient(f"mongodb+srv://carloswebdev:{mongo_pass_env}@micrologflask.1owutpp.mongodb.net/test")
+    app.db = client.microlog
+
+    @app.route("/", methods=["GET", "POST"])
+    def home_page():
+        if request.method == "POST":
+            log_content = request.form.get("log")
+            # log_information = (log_content, datetime.now().strftime("%m/%d/%y"), datetime.now().strptime(datetime.now().strftime("%m/%d/%y"), "%m/%d/%y"))
+            app.db.logs.insert_one({"log_entry": log_content, "log_date": datetime.now().strftime("%m/%d/%y %H:%M")})
+        kwargs = {
+            "log_info": [entry for entry in app.db.logs.find({})],
+        }
+        return render_template("index.html", **kwargs)
+
+
+    # Here it will have a dynamic url passing the slug of the thread title
+    
+    @app.route("/thread")
+    def thread():
+        return render_template("thread.html")
+
+
+    return app
+
+
