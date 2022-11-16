9b2cd11510b63707bc310e1b8bf215b60a7c53a3
Carlos García <carlosgarcia@Carloss-MacBook-Air.local>
2022-11-16 11:24:11 -0500

    endpoint for homepage rendering kwargs based on the post/get request from the log form submission. Next TODO incorporate mongodb to save logs and make CRUD calls

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from datetime import datetime
 
 app = Flask(__name__)
 
+entries_list = []
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home_page():
-    return render_template("index.html")
+    if request.method == "POST":
+        log_content = request.form.get("log")
+        log_information = (log_content, datetime.now().strftime("%m/%d/%y"), datetime.now().strptime(datetime.now().strftime("%m/%d/%y"), "%m/%d/%y"))
+        entries_list.append(log_information)
+    kwargs = {
+        "log_info": entries_list,
+    }
+    return render_template("index.html", **kwargs)
 
 
 # Here it will have a dynamic url passing the slug of the thread title
 @app.route("/thread")
 def thread():
     return render_template("thread.html")
+
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=4000)
